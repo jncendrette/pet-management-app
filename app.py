3b10ebc724c14c867e8eff8e963354c823b05714
José <jnc.vip@gmail.com>
2024-09-09 23:33:38 -0300

    :sparkles: feat: Arquivo atualizado

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Lista para armazenar os dados dos pets temporariamente
+pets = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', pets=pets)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    if request.method == 'POST':
+        # Coletar as informações do formulário
+        nome = request.form['nome']
+        idade = request.form['idade']
+        peso = request.form['peso']
+        tipo = request.form['tipo']
+
+        # Armazenar os dados do pet na lista
+        pets.append({
+            'peso': nome,
+            'idade': idade,
+            'peso': peso,
+            'tipo': tipo
+        })
+
+        return redirect(url_for('index'))
+
+    return render_template('add_pet.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
