dc90a74755ab157b449a0f0e45b230d7b1049270
José <jnc.vip@gmail.com>
2024-09-09 21:16:52 -0300

    :zap: perf: Separado em funções para melhorar performance

--- mypet.py
+++ mypet.py
@@ -46,8 +46,8 @@ def coletar_tipo_pet():
         print("Tipo inválido. Tente novamente.")
         return coletar_tipo_pet()
 
-def coletar_informacoes_pet():
-    """Função principal que coleta todas as informações sobre o pet."""
+# Função principal que coleta todas as informações do pet
+def main_pet():
     print("Por favor, insira as informações sobre seu pet.")
     
     nome = coletar_nome_pet()
@@ -55,5 +55,12 @@ def coletar_informacoes_pet():
     peso = coletar_peso_pet()
     tipo = coletar_tipo_pet()
 
+    # Exibindo as informações coletadas
+    print("\nInformações do pet:")
+    print(f"Nome: {nome}")
+    print(f"Idade: {idade} anos")
+    print(f"Peso: {peso} kg")
+    print(f"Tipo: {tipo}")
+
 # Chama a função para coletar e exibir as informações do pet
-coletar_informacoes_pet()
+main_pet()
